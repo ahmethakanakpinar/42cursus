c78bbc9371b6fd6d80fa03f18f49ce07854b7d5d
Ahmet Hakan Akpinar <aakpinar@k1m15s05.42istanbul.com.tr>
2024-10-08 10:34:54 +0300

    create not.txt

--- libft.h
+++ libft.h
@@ -6,12 +6,13 @@
 /*   By: aakpinar <aakpinar@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 17:26:26 by aakpinar          #+#    #+#             */
-/*   Updated: 2024/10/07 18:52:26 by aakpinar         ###   ########.fr       */
+/*   Updated: 2024/10/08 10:32:56 by aakpinar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 
