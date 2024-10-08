4ad0622ad1c0aa51fd4d52aaa6a1ba8414ddc8e8
Ahmet Hakan Akpinar <aakpinar@k1m15s05.42istanbul.com.tr>
2024-10-08 16:08:50 +0300

    ft_memset finished

---
+++ ft_memset.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aakpinar <aakpinar@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 10:36:18 by aakpinar          #+#    #+#             */
+/*   Updated: 2024/10/08 16:02:49 by aakpinar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*ptr;
+
+	ptr = (unsigned char *)s;
+	while (n--)
+		*ptr++ = (unsigned char)c;
+	return (s);
+}
