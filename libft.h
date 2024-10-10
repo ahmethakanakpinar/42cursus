a2ab56ca170b1075bc1d2079071648baaaac7970
Ahmet Hakan Akpinar <aakpinar@k1m15s05.42istanbul.com.tr>
2024-10-11 02:09:22 +0300

    ft_toupper finished

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aakpinar <aakpinar@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 17:26:26 by aakpinar          #+#    #+#             */
-/*   Updated: 2024/10/11 01:29:28 by aakpinar         ###   ########.fr       */
+/*   Updated: 2024/10/11 02:00:22 by aakpinar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,5 @@ void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+int		ft_toupper(int c);
 #endif
