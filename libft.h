d6298f900edadf6b0045a7394801955d40d7d2cc
Ahmet Hakan Akpinar <aakpinar@k1m15s05.42istanbul.com.tr>
2024-10-10 13:32:50 +0300

    ft_memove finished

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aakpinar <aakpinar@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 17:26:26 by aakpinar          #+#    #+#             */
-/*   Updated: 2024/10/08 16:22:04 by aakpinar         ###   ########.fr       */
+/*   Updated: 2024/10/09 15:47:35 by aakpinar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,15 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-int		ft_isdigit(int c);
 int		ft_isalpha(int c);
+int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
-void	*ft_memset(void *s, int c, size_t n);
 size_t	ft_strlen(const char *str);
+void	*ft_memset(void *s, int c, size_t n);
+void    ft_bzero(void *s, size_t n);
+void    *ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
 
 #endif
