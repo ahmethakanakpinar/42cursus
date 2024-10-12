2865b46d18fb773217a1623c5132eed3ad8be34c
Ahmet Hakan Akpinar <aakpinar@k1m15s07.42istanbul.com.tr>
2024-10-12 18:47:00 +0300

    ft_strnstr finished

---
+++ ft_strnstr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aakpinar <aakpinar@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/12 15:11:58 by aakpinar          #+#    #+#             */
+/*   Updated: 2024/10/12 18:46:28 by aakpinar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	n;
+
+	if (*needle == 0)
+		return ((char *)haystack);
+	n = strlen(needle);
+	while (*haystack && n <= len)
+	{
+		if (*haystack == *needle && ft_strncmp(haystack, needle, n) == 0)
+			return ((char *)haystack);
+		++haystack;
+		--len;
+	}
+	return (NULL);
+}
