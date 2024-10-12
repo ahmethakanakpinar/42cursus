bf60400bdb3e6dcf534c572eafbfad2d9de89e1a
Ahmet Hakan Akpinar <aakpinar@k1m15s07.42istanbul.com.tr>
2024-10-12 15:10:59 +0300

    ft_memchr refactoring

--- ft_memchr.c
+++ ft_memchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aakpinar <aakpinar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aakpinar <aakpinar@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 14:00:46 by aakpinar          #+#    #+#             */
-/*   Updated: 2024/10/11 15:44:54 by aakpinar         ###   ########.fr       */
+/*   Updated: 2024/10/12 15:08:48 by aakpinar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,9 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	unsigned char	*ptr;
 
 	ptr = (unsigned char *)s;
-	c = (unsigned char)c;
-	while (n && *ptr != c)
-	{
-		++ptr;
-		--n;
-	}
+	while (n-- && *ptr != (unsigned char)c)
+		ptr++;
 	if (n)
 		return ((void *)ptr);
-	else
-		return (NULL);
+	return (NULL);
 }
