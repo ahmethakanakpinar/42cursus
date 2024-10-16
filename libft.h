3eb81a299a4d35e1bc4faec8daf8ba0c89a88fc6
Ahmet Hakan Akpinar <aakpinar@k1m15s04.42istanbul.com.tr>
2024-10-16 17:23:45 +0300

    ft_strjoin finished

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aakpinar <aakpinar@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 17:26:26 by aakpinar          #+#    #+#             */
-/*   Updated: 2024/10/16 15:40:15 by aakpinar         ###   ########.fr       */
+/*   Updated: 2024/10/16 17:14:15 by aakpinar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,6 @@ int		ft_atoi(const char *str);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
