1544c9225707cbe56a159589963f39e84054f3a7
Ahmet Hakan Akpinar <aakpinar@k1m15s04.42istanbul.com.tr>
2024-10-16 15:41:39 +0300

    ft_substr finished

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aakpinar <aakpinar@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 17:26:26 by aakpinar          #+#    #+#             */
-/*   Updated: 2024/10/14 12:51:45 by aakpinar         ###   ########.fr       */
+/*   Updated: 2024/10/16 15:40:15 by aakpinar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,5 +39,6 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
