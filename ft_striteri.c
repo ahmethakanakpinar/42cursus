48c83a323932d53511c3f4a39092bf43b5843ba6
Ahmet Hakan Akpinar <aakpinar@k1m16s05.42istanbul.com.tr>
2024-11-03 14:48:47 +0300

    ft_striteri finished

--- ft_striteri.c
+++ ft_striteri.c
@@ -1,42 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aakpinar <aakpinar@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/16 16:57:03 by aakpinar          #+#    #+#             */
-/*   Updated: 2024/11/02 17:49:44 by aakpinar         ###   ########.fr       */
+/*   Created: 2024/11/02 11:58:56 by aakpinar          #+#    #+#             */
+/*   Updated: 2024/11/02 17:50:20 by aakpinar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
-	char	*tab;
-	size_t	i;
-	size_t	len1;
-	size_t	len2;
+	int	len;
+	int	i;
 
-	if (!s1 || !s2)
-		return (NULL);
-	len1 = ft_strlen(s1);
-	len2 = ft_strlen(s2);
-	tab = (char *)malloc(sizeof(char) * ((len1 + len2) + 1));
-	if (!tab)
-		return (NULL);
+	if (!s)
+		return ;
 	i = 0;
-	while (i < len1)
+	len = ft_strlen(s);
+	while (i < len)
 	{
-		tab[i] = s1[i];
+		f(i, &s[i]);
 		i++;
 	}
-	while (i < (len1 + len2))
-	{
-		tab[i] = s2[i - len1];
-		i++;
-	}
-	tab[i] = '\0';
-	return (tab);
 }
