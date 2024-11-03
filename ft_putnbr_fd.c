6f98432322178a90a48aec13ac0c2f5073ddaeaf
Ahmet Hakan Akpinar <aakpinar@k1m16s05.42istanbul.com.tr>
2024-11-03 17:42:32 +0300

    ft_putnbr finished

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -1,18 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aakpinar <aakpinar@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/03 14:50:07 by aakpinar          #+#    #+#             */
-/*   Updated: 2024/11/03 16:43:42 by aakpinar         ###   ########.fr       */
+/*   Created: 2024/11/03 16:18:45 by aakpinar          #+#    #+#             */
+/*   Updated: 2024/11/03 17:41:29 by aakpinar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putchar_fd(char c, int fd)
+static void	make_job(long nb, int fd)
 {
-	write(fd, &c, 1);
+	if (nb < 0)
+	{
+		ft_putchar_fd('-', fd);
+		nb *= -1;
+	}
+	if (nb > 9)
+	{
+		make_job(nb / 10, fd);
+		make_job(nb % 10, fd);
+	}
+	else
+	{
+		ft_putchar_fd(nb + '0', fd);
+	}
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long	nb;
+
+	nb = n;
+	make_job(nb, fd);
 }
