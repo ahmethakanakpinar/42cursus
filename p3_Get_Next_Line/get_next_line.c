b55dd1d817cdbffa4308f458f064448d1223e70b
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2024-12-20 23:53:59 +0300

    get_next_line start

--- p3_Get_Next_Line/get_next_line.c
+++ p3_Get_Next_Line/get_next_line.c
@@ -6,7 +6,18 @@
 /*   By: aakpinar <aakpinar@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 23:26:25 by aakpinar          #+#    #+#             */
-/*   Updated: 2024/12/20 23:26:26 by aakpinar         ###   ########.fr       */
+/*   Updated: 2024/12/20 23:53:29 by aakpinar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	char		*mainline;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	
+	return (mainline);
+}
--- p3_Get_Next_Line/get_next_line.h
+++ p3_Get_Next_Line/get_next_line.h
@@ -6,16 +6,21 @@
 /*   By: aakpinar <aakpinar@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 23:26:17 by aakpinar          #+#    #+#             */
-/*   Updated: 2024/12/20 23:27:33 by aakpinar         ###   ########.fr       */
+/*   Updated: 2024/12/20 23:53:30 by aakpinar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4
+# endif
+
 # include <unistd.h>
 # include <stdlib.h>
 
-
+char	*get_main_line(char *putline);
 
 #endif
