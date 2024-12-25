f8e4931ccbc9fc9f0c33609c5aa5ab1dd992c877
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2024-12-25 19:54:03 +0300

    Write get_next_line

--- p3_Get_Next_Line/get_next_line.h
+++ p3_Get_Next_Line/get_next_line.h
@@ -6,13 +6,12 @@
 /*   By: aakpinar <aakpinar@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 23:26:17 by aakpinar          #+#    #+#             */
-/*   Updated: 2024/12/20 23:53:30 by aakpinar         ###   ########.fr       */
+/*   Updated: 2024/12/25 19:53:13 by aakpinar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 4
@@ -21,6 +20,12 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*get_putremain(char *putline);
 char	*get_main_line(char *putline);
+char	*get_read(int fd, char *putline);
+char	*get_next_line(int fd);
 
 #endif
