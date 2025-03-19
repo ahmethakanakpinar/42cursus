59bb1143b7b37248bcad4402f3165e8aa68bb5df
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2025-03-19 22:09:55 +0300

    Continue

---
+++ p6_So_Long/free_utils.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aakpinar <aakpinar@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/19 20:57:29 by aakpinar          #+#    #+#             */
+/*   Updated: 2025/03/19 20:58:14 by aakpinar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	free_blocks(t_game *game)
+{
+	if (game->block.exit)
+		mlx_destroy_image(game->mlx_init, game->block.exit);
+	if (game->block.collectibles)
+		mlx_destroy_image(game->mlx_init, game->block.collectibles);
+	if (game->block.player)
+		mlx_destroy_image(game->mlx_init, game->block.player);
+	if (game->block.wall)
+		mlx_destroy_image(game->mlx_init, game->block.wall);
+	if (game->block.floor)
+		mlx_destroy_image(game->mlx_init, game->block.floor);
+}
+
+void	free_matrix(char **matrix)
+{
+	size_t	i;
+
+	i = 0;
+	if (!matrix)
+		return ;
+	while (matrix[i])
+	{
+		free(matrix[i]);
+		matrix[i] = NULL;
+		i++;
+	}
+	if (i > 0)
+		free(matrix);
+	matrix = NULL;
+}
+
+void	error_free(t_game *game)
+{
+	if (!game)
+		return ;
+	free_blocks(game);
+	if (game->mlx_wind)
+		mlx_destroy_window(game->mlx_init, game->mlx_wind);
+	if (game->mlx_init)
+	{
+		mlx_destroy_display(game->mlx_init);
+		free(game->mlx_init);
+	}
+	if (game->map.map)
+		free_matrix(game->map.map);
+}
+
+void	error_free_msg(t_game *game, char *error_msg)
+{
+	error_free(game);
+	ft_putendl_fd(error_msg, STDERR_FILENO);
+	exit(EXIT_FAILURE);
+}
--- p6_So_Long/so_long.h
+++ p6_So_Long/so_long.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aakpinar <aakpinar@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/19 20:35:56 by aakpinar          #+#    #+#             */
+/*   Updated: 2025/03/19 22:02:25 by aakpinar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
@@ -8,7 +20,7 @@
 # include <stdbool.h>
 # include <fcntl.h>
 
-# define BLOCK_SIZE 64
+# define BLOCK_SIZE 32
 
 
 typedef struct s_position
@@ -47,11 +59,26 @@ typedef struct s_game
 }				t_game;
 
 t_game			init_game(void);
+void	get_map(char *file_map, t_game *game);
 void	read_row(char *map_file, t_game *game);
 void	get_row(char *map_file, t_game *game);
 char	*trim_free(char *s1, char const *set);
 size_t	ft_strlen_endl(char *str);
 char	*f_strchr(const char *str, int c);
 char	*ft_strncpy(char *dest, const char *src, size_t n);
-
+int	check_file_extension(char *map_file);
+int	check_bor(t_map *map);
+int	check_str(char *str, int c);
+int	check_rect(t_game *game);
+char	**copy_map(t_game *game);
+int	flood_fill(t_map *map, t_position curr, char **temp_map);
+void	reset_flood_fill(t_map *map, t_position start, char **temp_map, int *result);
+void	check_path(t_game *game);
+void	check_ecp(t_game *game);
+void	check_count_el(t_game *game);
+void	unioncheck(t_game *game);
+void	free_blocks(t_game *game);
+void	free_matrix(char **matrix);
+void	error_free(t_game *game);
+void	error_free_msg(t_game *game, char *error_msg);
 #endif
