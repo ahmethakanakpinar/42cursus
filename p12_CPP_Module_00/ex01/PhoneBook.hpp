203a832b81289e2e2294f8f6dd2c86486e161c7b
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2026-01-02 04:42:30 +0300

    Kütüphaneler dahil edildi.

--- p12_CPP_Module_00/ex01/PhoneBook.hpp
+++ p12_CPP_Module_00/ex01/PhoneBook.hpp
@@ -6,12 +6,15 @@
 /*   By: aakpinar < aakpinar@student.42istanbul.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/02 04:33:33 by aakpinar          #+#    #+#             */
-/*   Updated: 2026/01/02 04:37:17 by aakpinar         ###   ########.fr       */
+/*   Updated: 2026/01/02 04:42:07 by aakpinar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
 #define PHONEBOOK_HPP
+
+#include <iostream>
+#include <string>
 
 #define RED "\033[31m"
 #define GREEN "\033[32m"
