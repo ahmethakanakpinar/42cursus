1548d8b9e6d8b9a88b8beebf290619943591adec
Ahmet Hakan Akpınar <ahmethakan.akpinar@gmail.com>
2026-01-02 04:29:49 +0300

    Kodu Refactore edip ENUM lar ile düzenledim.

--- p12_CPP_Module_00/ex01/main.cpp
+++ p12_CPP_Module_00/ex01/main.cpp
@@ -1,33 +1,55 @@
+enum Command
+{
+    ADD,
+    SEARCH,
+    EXIT,
+    INVALID
+};
+
+Command parseCommand(const std::string& input)
+{
+    if (input == "ADD")
+        return ADD;
+    else if (input == "SEARCH")
+        return SEARCH;
+    else if (input == "EXIT")
+        return EXIT;
+    return INVALID;
+}
+
 int main()
 {
     PhoneBook phoneBook;
-    std::string command;
+    std::string input;
+    Command command;
 
     std::cout << "Welcome to the PhoneBook!\n";
-
 
     do
     {
         std::cout << "\nEnter command (ADD, SEARCH, EXIT): ";        
-        std::getline(std::cin, command);
+        std::getline(std::cin, input);
 
         if(std::cin.eof())
             break;
         
-        if (command == "ADD")
-        {
-          
-        }
-        else if (command == "SEARCH")
-        {
-         
-        }
-        else if (command != "EXIT")
-        {
-            std::cout << RED << "Invalid command. Please try again." << RESET << std::endl;
-        }
-    } while (command != "EXIT");
+        command = parseCommand(input);
 
+        switch (command)
+        {
+            case ADD:
+                // ADD logic here
+                break;
+            case SEARCH:
+                // SEARCH logic here
+                break;
+            case EXIT:
+                break;
+            case INVALID:
+                std::cout << RED << "Invalid command. Please try again." << RESET << std::endl;
+                break;
+        }
+    } while (command != EXIT);
     std::cout << GREEN << "Goodbye!" << RESET << std::endl;
     return(0);
 }
