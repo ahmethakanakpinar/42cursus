480d6afb67156ef40711f449c486150706953775
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2026-01-28 03:12:10 +0300

    Zombie sınıfı eklendi; oluşturma ve yok etme yöntemleri ile duyuru işlevi tanımlandı.

--- p13_CPP_Module_01/Zombie.hpp
+++ p13_CPP_Module_01/Zombie.hpp
@@ -6,7 +6,31 @@
 /*   By: aakpinar <aakpinar@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 01:12:54 by aakpinar          #+#    #+#             */
-/*   Updated: 2026/01/22 01:13:57 by aakpinar         ###   ########.fr       */
+/*   Updated: 2026/01/28 03:11:23 by aakpinar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <string>
+#include <iostream>
+
+
+class Zombie
+{
+    private:
+        std::string _name;
+
+    public:
+        Zombie(std::string name);
+        ~Zombie();
+        void announce( void );
+
+};
+Zombie* createZombie(std::string name);
+
+
+
+#endif
