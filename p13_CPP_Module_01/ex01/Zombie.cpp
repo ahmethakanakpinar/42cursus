c32bbdde82f573bf36bffe62662ad5d4d49ece03
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2026-01-28 03:51:22 +0300

    Hata almamak için Default Constructor da tanımlandı.

---
+++ p13_CPP_Module_01/ex01/Zombie.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aakpinar <aakpinar@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/28 03:47:14 by aakpinar          #+#    #+#             */
+/*   Updated: 2026/01/28 03:50:42 by aakpinar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie::Zombie()
+{
+    this->_name = "";
+    std::cout << "Zombie empty created." << std::endl;
+}
+
+Zombie::Zombie(std::string name)
+{
+    this->_name = name;
+    std::cout << "Zombie " << this->_name << " is created." << std::endl;
+}
+
+Zombie::~Zombie()
+{
+    std::cout << "Zombie " << this->_name << " is destroyed." << std::endl;
+}
+void Zombie::announce(void)
+{
+    std::cout << this->_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
