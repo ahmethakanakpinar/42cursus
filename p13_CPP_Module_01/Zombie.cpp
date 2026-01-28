63a3b731797db01a2a45f5276af37ff0c0e769bf
Ahmet Hakan Akpınar <ahmethakan.akpinar@gmail.com>
2026-01-28 03:12:10 +0300

    Zombie sınıfı eklendi; oluşturma ve yok etme yöntemleri ile duyuru işlevi tanımlandı.

--- p13_CPP_Module_01/Zombie.cpp
+++ p13_CPP_Module_01/Zombie.cpp
@@ -6,7 +6,23 @@
 /*   By: aakpinar <aakpinar@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 01:12:59 by aakpinar          #+#    #+#             */
-/*   Updated: 2026/01/22 01:12:59 by aakpinar         ###   ########.fr       */
+/*   Updated: 2026/01/28 03:12:00 by aakpinar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
+
+Zombie::Zombie(std::string name)
+{
+    this->_name = name;
+    std::cout << "Zombie " << this->_name << " is created." << std::endl;
+}
+
+Zombie::~Zombie()
+{
+    std::cout << "Zombie " << this->_name << " is destroyed." << std::endl;
+}
+void Zombie::announce( void )
+{
+    std::cout << this->_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
