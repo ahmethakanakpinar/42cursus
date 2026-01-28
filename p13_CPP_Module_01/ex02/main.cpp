9cf9d8e4218ae3a856663c75d14b5f77ab06b9b8
Ahmet Hakan Akpınar <ahmethakan.akpinar@gmail.com>
2026-01-28 04:18:26 +0300

    Update Makefile for ex01 and add main.cpp for ex02

---
+++ p13_CPP_Module_01/ex02/main.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aakpinar <aakpinar@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/28 04:12:11 by aakpinar          #+#    #+#             */
+/*   Updated: 2026/01/28 04:18:00 by aakpinar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string str = "HI THIS IS BRAIN";
+
+    std::string* stringPTR = &str;
+
+    std::string& stringREF = str;
+
+    // Print memory addresses
+    std::cout << "Memory address of the string variable: " << &str << std::endl;
+    std::cout << "Memory address held by stringPTR:      " << stringPTR << std::endl;
+    std::cout << "Memory address held by stringREF:      " << &stringREF << std::endl;
+
+    std::cout << "----------------------------------------" << std::endl;
+    
+    // Print values
+    std::cout << "Value of the string variable:  " << str << std::endl;
+    std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+
+    return 0;
+}
