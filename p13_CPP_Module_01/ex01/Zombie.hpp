af9f2a82bc9a3bb8ef0e160e280aed2cc981d4c8
Ahmet Hakan Akpınar <ahmethakan.akpinar@gmail.com>
2026-01-28 03:51:22 +0300

    Hata almamak için Default Constructor da tanımlandı.

---
+++ p13_CPP_Module_01/ex01/Zombie.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aakpinar <aakpinar@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/28 03:47:02 by aakpinar          #+#    #+#             */
+/*   Updated: 2026/01/28 03:50:45 by aakpinar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+
+
+class Zombie
+{
+    private:
+        std::string _name;
+
+    public:
+        Zombie();
+        Zombie(std::string name);
+        ~Zombie();
+        void announce(void);
+
+};
+Zombie* newZombie(std::string name);
+void randomChump(std::string name);
+
+#endif
