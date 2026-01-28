becd2c9b22c70a90f6a18f093337bf34f6fccc06
Ahmet Hakan Akpınar <ahmethakan.akpinar@gmail.com>
2026-01-28 04:10:36 +0300

    Main yazıldı ex01 bitti

--- p13_CPP_Module_01/ex01/main.cpp
+++ p13_CPP_Module_01/ex01/main.cpp
@@ -6,7 +6,21 @@
 /*   By: aakpinar <aakpinar@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/28 03:53:04 by aakpinar          #+#    #+#             */
-/*   Updated: 2026/01/28 04:00:39 by aakpinar         ###   ########.fr       */
+/*   Updated: 2026/01/28 04:10:16 by aakpinar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
+
+int main()
+{
+    int count = 10;
+    Zombie* zombies = zombieHorde(count, "Zombie");
+    for(int i = 0; i < count; i++)
+    {
+        zombies[i].announce();
+    }
+
+    delete[] zombies;
+    return 0;
+}
