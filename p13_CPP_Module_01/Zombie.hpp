0e01709bf87d683f6d4c9b85700f4b8ce794c6e3
Ahmet Hakan Akpınar <ahmethakan.akpinar@gmail.com>
2026-01-28 03:31:36 +0300

    pdf te fonksiyon ismi zorunluluğu var mış newZombie olarak düzelttim

--- p13_CPP_Module_01/Zombie.hpp
+++ p13_CPP_Module_01/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: aakpinar <aakpinar@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 01:12:54 by aakpinar          #+#    #+#             */
-/*   Updated: 2026/01/28 03:15:17 by aakpinar         ###   ########.fr       */
+/*   Updated: 2026/01/28 03:31:02 by aakpinar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ class Zombie
         void announce(void);
 
 };
-Zombie* createZombie(std::string name);
+Zombie* newZombie(std::string name);
+void randomChump( std::string name );
 
 #endif
--- p13_CPP_Module_01/main.cpp
+++ p13_CPP_Module_01/main.cpp
@@ -6,7 +6,21 @@
 /*   By: aakpinar <aakpinar@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 01:10:33 by aakpinar          #+#    #+#             */
-/*   Updated: 2026/01/22 01:10:33 by aakpinar         ###   ########.fr       */
+/*   Updated: 2026/01/28 03:31:12 by aakpinar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
+
+
+int main()
+{
+    Zombie* NewZombie = newZombie("Foo");
+    NewZombie->announce();
+    delete NewZombie;
+
+    Zombie zombie("Foo");
+    zombie.announce();
+    randomChump("Foo");
+    return 0;
+}
