3efd2a4a3e3e57b11f6407456c5bc8839e592aee
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2026-02-08 11:58:58 +0300

    Fix parameter passing by using const reference for Weapon class constructor and setType method

--- p13_CPP_Module_01/ex03/Weapon.hpp
+++ p13_CPP_Module_01/ex03/Weapon.hpp
@@ -6,9 +6,9 @@
 class Weapon
 {
   public:
-	Weapon(const std::string type);
+	Weapon(const std::string& type);
 	const std::string& getType() const;
-	void setType(const std::string newType);
+	void setType(const std::string& newType);
 
   private:
 	std::string _type;
