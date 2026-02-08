d3a0fad780fc9d727b4fddc48d4ef1760a972aa5
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2026-02-08 12:14:00 +0300

    Implement HumanB class with setWeapon and attack methods; update main to demonstrate usage

---
+++ p13_CPP_Module_01/ex03/HumanB.hpp
@@ -0,0 +1,20 @@
+#ifndef HUMANB_HPP
+#define HUMANB_HPP
+
+#include <string>
+#include "Weapon.hpp"
+#include <iostream>
+
+class HumanB
+{
+  public:
+	HumanB(const std::string &name);
+	void setWeapon(Weapon &weapon);
+	void attack() const;
+
+  private:
+	std::string _name;
+	Weapon *_weapon;
+};
+
+#endif
