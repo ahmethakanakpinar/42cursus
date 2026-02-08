03e25616225be5147f493d5d885e40c622563859
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2026-02-08 12:06:37 +0300

    Implement HumanA class with constructor and attack method

---
+++ p13_CPP_Module_01/ex03/HumanA.hpp
@@ -0,0 +1,20 @@
+#ifndef HUMANA_HPP
+#define HUMANA_HPP
+
+#include <string>
+#include "Weapon.hpp"
+#include <iostream>
+
+class HumanA
+{
+
+public:
+    HumanA(const std::string& name, Weapon& weapon);
+    void attack() const;
+
+private:
+    std::string _name;
+    Weapon& _weapon;
+};
+
+#endif
