1fbc14cf3c7c6435e42c91b3709f9da44544fbdf
Ahmet Hakan Akpınar <ahmethakan.akpinar@gmail.com>
2026-02-23 15:45:56 +0300

    Implement Fixed class with constructor, destructor, and methods for raw bits manipulation

--- p14_CPP_Module_02/Fixed.cpp
+++ p14_CPP_Module_02/Fixed.cpp
@@ -1,2 +1,21 @@
 #include "Fixed.hpp"
 
+Fixed::Fixed() : _rawBits(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return _rawBits;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    std::cout << "setRawBits member function called" << std::endl;
+	_rawBits = raw;
+}
