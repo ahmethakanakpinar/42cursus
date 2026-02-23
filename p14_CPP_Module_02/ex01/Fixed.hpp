89a29dcbb80b0d7fdfdc16989969de02b9de90e5
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2026-02-23 16:53:38 +0300

    Implement Fixed class with methods for raw bits manipulation and update main function to demonstrate functionality

---
+++ p14_CPP_Module_02/ex01/Fixed.hpp
@@ -0,0 +1,30 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed
+{
+public:
+	Fixed();
+	~Fixed();
+    Fixed(const Fixed &other);
+    Fixed(const int value);
+	Fixed(const float value);
+	Fixed &operator=(const Fixed &other);
+
+    float toFloat(void) const;
+	int toInt(void) const;
+
+    int getRawBits(void) const;
+	void setRawBits(int const raw);
+
+private:
+	int _rawBits;
+	static const int _fractionalBits = 8;
+};
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed);
+
+
+#endif
