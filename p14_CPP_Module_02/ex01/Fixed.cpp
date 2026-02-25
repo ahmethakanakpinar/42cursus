4cced301b87f36cf249749f3ca7f9046751aaa35
Ahmet Hakan Akpınar <ahmethakan.akpinar@gmail.com>
2026-02-25 10:32:01 +0300

    Refactor Fixed class constructors and destructors for consistency and clarity

--- p14_CPP_Module_02/ex01/Fixed.cpp
+++ p14_CPP_Module_02/ex01/Fixed.cpp
@@ -5,41 +5,36 @@ Fixed::Fixed() : _rawBits(0)
 {
 	std::cout << "Default constructor called" << std::endl;
 }
+
+Fixed::Fixed(const Fixed &other) : _rawBits(0)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
+
+Fixed::Fixed(const int value) : _rawBits(value << _fractionalBits)
+{
+	std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float value) : _rawBits(roundf(value * (1 << _fractionalBits)))
+{
+	std::cout << "Float constructor called" << std::endl;
+}
+
 Fixed::~Fixed()
 {
-    std::cout << "Destructor called" << std::endl;
+	std::cout << "Destructor called" << std::endl;
 }
-Fixed& Fixed::operator=(const Fixed& other)
+
+Fixed &Fixed::operator=(const Fixed &other)
 {
 	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &other)
-	{
 		_rawBits = other.getRawBits();
-	}
 	return (*this);
 }
 
-Fixed::Fixed(const int value) 
-{
-	std::cout << "Int constructor called" << std::endl;
-	_rawBits = value << _fractionalBits;
-}
-
-Fixed::Fixed(const float value)
-{
-	std::cout << "Float constructor called" << std::endl;
-	_rawBits = roundf(value * (1 << _fractionalBits));
-}
-
-int Fixed::getRawBits(void) const
-{
-	return (_rawBits);
-}
-
-void Fixed::setRawBits(int const raw)
-{
-	_rawBits = raw;
-}
 float Fixed::toFloat(void) const
 {
 	return (static_cast<float>(_rawBits) / (1 << _fractionalBits));
@@ -50,10 +45,14 @@ int Fixed::toInt(void) const
 	return (_rawBits >> _fractionalBits);
 }
 
-Fixed::Fixed(const Fixed& other)
+int Fixed::getRawBits(void) const
 {
-    std::cout << "Copy constructor called" << std::endl;
-    _rawBits = other.getRawBits();
+	return (_rawBits);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	_rawBits = raw;
 }
 
 std::ostream &operator<<(std::ostream &ost, const Fixed &fixed)
