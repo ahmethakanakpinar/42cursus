97f8b76896e88e3e14c62e1dfa826732fa2c5949
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2026-03-04 15:29:01 +0300

    ex00 tamamlandı

---
+++ p16_CPP_Module_04/ex00/WrongCat.cpp
@@ -0,0 +1,35 @@
+#include "WrongCat.hpp"
+#include <iostream>
+
+WrongCat::WrongCat()
+{
+    this->type = "WrongCat";
+    std::cout << "WrongCat default constructor called" << std::endl;
+}
+
+WrongCat::WrongCat(const WrongCat& other) : WrongAnimal(other)
+{
+    std::cout << "WrongCat copy constructor called" << std::endl;
+    this->type = other.type;
+}
+
+WrongCat& WrongCat::operator=(const WrongCat& other)
+{
+    std::cout << "WrongCat copy assignment operator called" << std::endl;
+    if (this != &other)
+    {
+        WrongAnimal::operator=(other);
+        this->type = other.type;
+    }
+    return *this;
+}
+
+WrongCat::~WrongCat()
+{
+    std::cout << "WrongCat destructor called" << std::endl;
+}
+
+void WrongCat::makeSound() const
+{
+    std::cout << "Meow? (wrong cat)" << std::endl;
+}
--- p16_CPP_Module_04/ex00/main.cpp
+++ p16_CPP_Module_04/ex00/main.cpp
@@ -1,3 +1,85 @@
+#include <iostream>
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
+
+
 int main()
 {
+    {
+        const Animal* meta = new Animal();
+        const Animal* j = new Dog();
+        const Animal* i = new Cat();
+
+        std::cout << j->getType() << std::endl;
+        std::cout << i->getType() << std::endl;
+
+        i->makeSound();
+        j->makeSound();
+        meta->makeSound();
+
+
+        delete meta;
+        delete j;
+        delete i;
+    }
+
+    {
+        Dog d;
+        Cat c;
+
+        const Animal& a1 = d;
+        const Animal& a2 = c;
+
+        std::cout << "a1 type: " << a1.getType() << std::endl;
+        std::cout << "a2 type: " << a2.getType() << std::endl;
+
+        a1.makeSound();
+        a2.makeSound();
+    }
+
+    {
+        Dog d1;
+        Dog d2(d1);
+        Dog d3;
+        d3 = d1;
+
+        Cat c1;
+        Cat c2(c1);
+        Cat c3;
+        c3 = c1;
+
+        std::cout << "d1 type: " << d1.getType() << std::endl;
+        std::cout << "d2 type: " << d2.getType() << std::endl;
+        std::cout << "d3 type: " << d3.getType() << std::endl;
+
+        std::cout << "c1 type: " << c1.getType() << std::endl;
+        std::cout << "c2 type: " << c2.getType() << std::endl;
+        std::cout << "c3 type: " << c3.getType() << std::endl;
+
+        d1.makeSound();
+        c1.makeSound();
+    }
+
+    {
+        const WrongAnimal* wa = new WrongAnimal();
+        const WrongAnimal* wc_as_wa = new WrongCat();
+
+        std::cout << "wa type: " << wa->getType() << std::endl;
+        std::cout << "wc_as_wa type: " << wc_as_wa->getType() << std::endl;
+
+        wa->makeSound();
+        wc_as_wa->makeSound();
+
+        const WrongCat* wc = new WrongCat();
+        wc->makeSound();
+
+        delete wa;
+        delete wc_as_wa;
+        delete wc;
+    }
+
+    return 0;
 }
