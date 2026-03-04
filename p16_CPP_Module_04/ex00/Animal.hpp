be403cc3bd1baa51b9802e47a6c511c5b04f8154
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2026-03-04 11:51:23 +0300

    Hpp dosyaları yazıldı

---
+++ p16_CPP_Module_04/ex00/Animal.hpp
@@ -0,0 +1,24 @@
+#ifndef ANIMAL_HPP
+#define ANIMAL_HPP
+
+#include <iostream>
+#include <string>
+
+class Animal 
+{
+
+public:
+    Animal();
+    Animal(const Animal& src);
+    Animal& operator=(const Animal& src);
+    virtual ~Animal();
+
+    virtual void makeSound() const;
+    std::string getType() const;
+
+protected:
+    std::string type;
+
+};
+
+#endif
