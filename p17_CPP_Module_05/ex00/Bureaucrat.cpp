a2693e25ad59cb271fe11cfe3655d8add4fbff52
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2026-03-10 01:56:50 +0300

    Implement increment and decrement methods for Bureaucrat class

--- p17_CPP_Module_05/ex00/Bureaucrat.cpp
+++ p17_CPP_Module_05/ex00/Bureaucrat.cpp
@@ -29,6 +29,20 @@ int Bureaucrat::getGrade() const
     return _grade;
 }
 
+void Bureaucrat::incrementGrade()
+{
+    if (_grade - 1 < 1)
+        throw GradeTooHighException();
+    _grade--;     
+}
+
+void Bureaucrat::decrementGrade()
+{
+    if (_grade + 1 > 150)
+        throw GradeTooLowException();
+    _grade++;     
+}
+
 const char* Bureaucrat::GradeTooHighException::what() const throw()
 {
     return "Grade is too high, cannot be less than 1";
