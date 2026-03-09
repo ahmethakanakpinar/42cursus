adb255b36e19babff08c3047aa9d2c813e3ed9ce
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2026-03-10 01:50:14 +0300

    Implement Bureaucrat class with constructors, destructor, and accessor methods

--- p17_CPP_Module_05/ex00/Bureaucrat.cpp
+++ p17_CPP_Module_05/ex00/Bureaucrat.cpp
@@ -1,1 +1,30 @@
 #include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : _name("Default"), _grade(150)
+{
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other) : _name(other._name), _grade(other._grade)
+{
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat &other) 
+{
+    if (this != &other) 
+    {
+        _grade = other._grade;
+    }
+    return (*this);
+}
+Bureaucrat::~Bureaucrat()
+{
+}
+const std::string& Bureaucrat::getName() const
+{
+    return _name;
+}
+
+int Bureaucrat::getGrade() const
+{
+    return _grade;
+}
