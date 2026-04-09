c484af597eb48d9b224b702bd3385ce78066245e
Ahmet Hakan Akpınar <ahmethakan_52@hotmail.com>
2026-04-09 17:48:54 +0300

    feat: implement ScalarConverter class structure with canonical form members

---
+++ p18_CPP_Module_06/ex00/ScalarConverter.cpp
@@ -0,0 +1,6 @@
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter(void) {}
+ScalarConverter::ScalarConverter(const ScalarConverter &src) { (void)src; }
+ScalarConverter &ScalarConverter::operator=(const ScalarConverter &rhs) { (void)rhs; return *this; }
+ScalarConverter::~ScalarConverter(void) {}
