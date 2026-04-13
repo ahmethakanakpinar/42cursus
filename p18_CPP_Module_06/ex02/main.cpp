cab79acd99d7e5a3297ea269b5697f2c5c8b9b09
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2026-04-13 03:44:51 +0300

    Makefile dosyalar ve hpp ler yazıldı.

---
+++ p18_CPP_Module_06/ex02/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+
+int main(void)
+{
+	std::srand(std::time(NULL));
+
+	for (int i = 0; i < 5; i++)
+	{
+		
+	}
+	return 0;
+}
