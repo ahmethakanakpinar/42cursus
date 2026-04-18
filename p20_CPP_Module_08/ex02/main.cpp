a1a9a6a3368a92cd106561de187ac8e25b31d0f3
ahmethakanakpinar <ahmethakan_52@hotmail.com>
2026-04-18 15:51:21 +0300

    cpp08 bitti gibi

--- p20_CPP_Module_08/ex02/main.cpp
+++ p20_CPP_Module_08/ex02/main.cpp
@@ -4,5 +4,68 @@
 
 int main(void)
 {
+	// --- Subject test ---
+	std::cout << "=== MutantStack ===" << std::endl;
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+
+	// --- Same test with std::list ---
+	std::cout << "\n=== std::list ===" << std::endl;
+	std::list<int> lst;
+	lst.push_back(5);
+	lst.push_back(17);
+	std::cout << lst.back() << std::endl;
+	lst.pop_back();
+	std::cout << lst.size() << std::endl;
+	lst.push_back(3);
+	lst.push_back(5);
+	lst.push_back(737);
+	lst.push_back(0);
+
+	std::list<int>::iterator lit = lst.begin();
+	std::list<int>::iterator lite = lst.end();
+	++lit;
+	--lit;
+	while (lit != lite)
+	{
+		std::cout << *lit << std::endl;
+		++lit;
+	}
+
+	// --- Reverse iterator test ---
+	std::cout << "\n=== Reverse iterator ===" << std::endl;
+	MutantStack<int>::reverse_iterator rit = mstack.rbegin();
+	MutantStack<int>::reverse_iterator rite = mstack.rend();
+	while (rit != rite)
+	{
+		std::cout << *rit << std::endl;
+		++rit;
+	}
+
+	// --- Copy test ---
+	std::cout << "\n=== Copy test ===" << std::endl;
+	MutantStack<int> copy(mstack);
+	std::cout << "Copy size: " << copy.size() << std::endl;
+	std::cout << "Copy top: " << copy.top() << std::endl;
+
 	return 0;
 }
